5b3e0769f3504adbc837c3b04d968d42276abd55
Gabriel Santos <coder.gabrielsantos@gmail.com>
2025-06-02 13:38:57 -0300

    add responsive media queries

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Cart, TransitionRight, LogIn, OpenNewWindow } from "iconoir-react";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import styles from "./Navbar.module.css";
 
 const Navbar = ({ onLoginClick, onCartClick }) => {
     const navigate = useNavigate();
@@ -12,34 +13,34 @@ const Navbar = ({ onLoginClick, onCartClick }) => {
     }, []);
 
     return (
-        <div style={styles.navWrapper}>
-            <nav style={styles.navbar}>
-                <h1 onClick={() => navigate("/")} style={styles.logo}>Santa Teresinha</h1>
+        <div className={styles["nav-wrapper"]}>
+            <nav className={styles.navbar}>
+                <h1 onClick={() => navigate("/")} className={styles.logo}>Santa Teresinha</h1>
 
-                <div style={styles.userActions}>
+                <div className={styles["user-actions"]}>
                     {user && user.role === "admin" ? (
-                        <button style={styles.actionBtn} onClick={() => navigate("/painel")}>
-                            <OpenNewWindow style={styles.icon} />
+                        <button className={styles["action-btn"]} onClick={() => navigate("/painel")}>
+                            <OpenNewWindow className={styles.icon} />
                             <span className="action-label">Painel do Administrador</span>
                         </button>
                     ) : (
                         user && (
-                            <button style={styles.actionBtn} onClick={onCartClick}>
-                                <Cart style={styles.icon} />
+                            <button className={styles["action-btn"]} onClick={onCartClick}>
+                                <Cart className={styles.icon} />
                                 <span className="action-label">Carrinho</span>
                             </button>
                         )
                     )}
 
                     {user ? (
-                        <button style={styles.actionBtn} onClick={() => navigate("/perfil")}>
-                            <TransitionRight style={styles.icon}/>
+                        <button className={styles["action-btn"]} onClick={() => navigate("/perfil")}>
+                            <TransitionRight className={styles.icon}/>
                             <span className="action-label">Minha Conta</span>
                         </button>
                     ) : (
-                        <div onClick={onLoginClick} style={styles.login}>
-                            <LogIn style={styles.icon}/>
-                            Entrar
+                        <div onClick={onLoginClick} className={styles.login}>
+                            <LogIn className={styles.icon}/>
+                            <span>Entrar</span>
                         </div>
                     )}
                 </div>
@@ -49,59 +50,3 @@ const Navbar = ({ onLoginClick, onCartClick }) => {
 };
 
 export default Navbar;
-
-const styles = {
-    actionBtn: {
-        alignItems: "center",
-        backgroundColor: "#fff",
-        border: "1px solid #ddd",
-        borderRadius: "10px",
-        color: "#333",
-        cursor: "pointer",
-        display: "flex",
-        fontSize: "0.95rem",
-        fontWeight: 500,
-        gap: "0.5rem",
-        padding: "0.4rem 0.8rem",
-        transition: "all 0.2s ease"
-    },
-    icon: {
-        fontSize: "1rem"
-    },
-    logo: {
-        cursor: "pointer",
-        fontSize: "1.2rem",
-        fontWeight: "700"
-    },
-    login: {
-        alignItems: "center",
-        color: "#4CAF50",
-        cursor: "pointer",
-        display: "flex",
-        fontSize: "0.95rem",
-        fontWeight: 500,
-        gap: "0.5rem",
-        textDecoration: "none"
-    },
-    navWrapper: {
-        backgroundColor: "#fff",
-        borderBottom: "1px solid #ccc",
-        position: "sticky",
-        top: 0,
-        zIndex: 100
-    },
-    navbar: {
-        alignItems: "center",
-        display: "flex",
-        justifyContent: "space-between",
-        margin: "0 auto",
-        maxWidth: "1440px",
-        padding: "1rem 2rem",
-        width: "100%"
-    },
-    userActions: {
-        display: "flex",
-        alignItems: "center",
-        gap: "1rem"
-    }
-};
